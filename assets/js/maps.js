7dcff96dbaeb0e544d073441428139df3c87aa7e
EC2 Default User <ec2-user@ip-172-31-42-123.ec2.internal>
2019-10-06 16:13:24 +0000

    added autocomplete of places

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,48 +6,6 @@ function initMap() {
         zoom: 10
       
     });
-    
-    var request = {
-        locations: center,
-        radius: 3000,
-        types: ['cafe']
-    };
-    
-    var service = new  google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-}
+}    
+google.maps.event.addDomListener(window, 'load', initMap);
 
-function callback(result, status){
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++){
-            createMarker(results[i]);
-        }
-    }
-}
-
-function createMarker(place) {
-    var placeLoc = place.geomerty.location;
-    var marker =new google.map.Marker({
-        map:map,
-        position: place.geomerty.location
-    });
-    
-google.map.event.addDomListener(window, 'load', initMap)
-}
-    
- /*   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
-        { lat: 40.785091, lng: -73.698285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984061 }
-    ];
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });*/
-    
-    /*image for the cluster
-         var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-         }*/
--- assets/js/packages.js
+++ assets/js/packages.js
@@ -1,83 +1,26 @@
-function showPackages(fetchHolType) {
-   var dest = document.getElementById("dest").value;
-   var holType = document.getElementById("holType").value;
-   
-   if (holType == "famGet") {
-       $(document).ready(function() {
-    $.get('fampack.html', function(data) {
-        $('#packages').html(data);
-    });
-});
-   }else if (holType == "romRet"){
-       $(document).ready(function() {
-    $.get('rompack.html', function(data) {
-        $('#packages').html(data);
-    });
-});
-   }
-   else if (holType == "friEsc"){
-       $(document).ready(function() {
-    $.get('escapePack.html', function(data) {
-        $('#packages').html(data);
-    });
-});
-   }
-   
+/*-----------------Autocomplete locations*/
+function activatePlacesSearch() {
+   var input = document.getElementById('search_term');
+   var Autocomplete = new google.maps.places.Autocomplete(input);
 }
 
-/*function fetchorigin() {
-     var origin = document.getElementById("origin").value;
-     console.log(origin);
-     }
-
-function fetchdest() {
-     var dest = document.getElementById("dest").value;
-     console.log(dest);
-     }
-
-function fetchtype() {
-     var type = document.getElementById("type").value;
-     console.log(type);
-     }
-
-/*$(document).ready(function() {
-    $.get('rompack.html', function(data) {
-        $('#romantic-packages').html(data);
-    });
-});
+/*-----------------Fetch and display packages to match search criteria*/
 
 
-if(dest ="paris"){
-   $(document).ready(function() {
-    $.get('rompack.html', function(data) {
-        $('#romantic-packages').html(data);
-    });
-});
-} else if (dest ="berlin"){
-    $(document).ready(function() {
-    $.get('fampack.html', function(data) {
-        $('#family-packages').html(data);
-    });
-});
+
+
+
+function fetchhotels(event) {
+
+
+
+    var username = $("#city").val();
+
+    if (!username) {
+
+        $("#hotelpic").html(`<h2>Please enter search criteria</h2>`);
+
+        return;
+
+    }
 }
-$('#romantic-packages').html('Value = ' + selectedHolType.val() + ", Text = " + selectedHolType.text());
-
-
-    $(document).ready(function() {
-        $('#holType').change(function() {
-            var selectedHolType = $('#holType option:selected');
-            $('#romantic-packages').html('Value = ' + selectedHolType.val() + ", Text = " + selectedHolType.text());;
-
-        });
-    });
-
-function fethdest() {
-     var dest = document.getElementById("dest").value;
-     return dest;
-     }
-
-function fetchHolType() {
-     var holType = document.getElementById("holType").value;
-     return holType;
-     }
-*/
